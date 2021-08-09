d1b19cb34d2fd3d3788d9405125c72c4ef661124
@koro90 <karina@goaxiomatic.com>
2021-08-09 17:20:26 -0400

    add new test - go to login page from product page

--- enviroments/selenium_env/bin/activate.fish
+++ enviroments/selenium_env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/karina/Desktop/final-project/enviroments/selenium_env"
+set -gx VIRTUAL_ENV "/Users/karina/Desktop/final-project-new/selenium_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- test_product_page.py
+++ test_product_page.py
@@ -9,15 +9,21 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
-
-def test_guest_can_add_product_to_cart(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
-    page.press_button_add_to_basket()
-    page.solve_quiz_and_get_code()
-    page.should_be_message_about_adding()
-    page.should_be_message_basket_total()
+    page.go_to_login_page()
+
+
+# def test_guest_can_add_product_to_cart(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.press_button_add_to_basket()
+#     page.solve_quiz_and_get_code()
+#     page.should_be_message_about_adding()
+#     page.should_be_message_basket_total()
 
     
 # @pytest.mark.xfail(reason="fixing this bug right now")
