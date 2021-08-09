c48a7824ed56f9433bb8f258c4ac86966d2bb521
@koro90 <karina@goaxiomatic.com>
2021-08-09 15:17:02 -0400

    new tests

---
+++ test_product_page.py
@@ -0,0 +1,37 @@
+
+from pages.product_page import ProductPage
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+
+def test_guest_can_add_product_to_cart(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.press_button_add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_message_about_adding()
+    page.should_be_message_basket_total()
+
+    
+# @pytest.mark.xfail(reason="fixing this bug right now")
+# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.press_button_add_to_basket()
+#     page.solve_quiz_and_get_code()
+#     page.should_not_be_success_message()
+
+# def test_guest_cant_see_success_message(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_not_be_success_message()
+
